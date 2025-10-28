809d86b06a51f74925f2daa57a3d14f2520a10e1
FabSignal <fabianafernandezf8@gmail.com>
2025-10-27 23:08:43 -0300

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
-// Context Provider
 import { CartProvider } from "./context/CartContext";
 
-// Se importa bootstrap antes que css
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./index.css";
-// import "./App.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <HashRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HashRouter>
   </StrictMode>
 );
